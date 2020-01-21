0184adf0b2c196425a55ad3343c156ce6c3bee12
Joseph <josephvisaac@gmail.com>
2020-01-21 14:50:00 -0500

    aight

--- codewar.js
+++ codewar.js
@@ -7,5 +7,16 @@ function positiveSum(arr) {
     })
     return count
    }
-
+   //silly changes haha
    //meow
+   // this is some code I was playing with 
+   let arr = [4,3,5,2,6,5,7,8,9,10,11,12];
+  let evens = [];
+arr.forEach( num => {
+
+  if(num % 2 == 0){ evens.push(num)
+  
+  }
+ 
+})
+console.log(evens.sort(function(a, b){return a-b}))
